7d7e350eba54162ecfa19ea130b02b298157b064
Sunny <Tianqing@ooooooo.local>
2014-07-03 11:22:44 +0800

    Initial Commit

---
+++ Agenda/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Agenda
+//
+//  Created by Sunny on 14-7-3.
+//  Copyright (c) 2014年 Sunny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
